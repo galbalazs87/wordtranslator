7deeef46790762009a432f5099142886040df3dc
galbalazs87 <galbalazs87@gmail.com>
2017-07-19 19:18:11 +0200

    Final version of my word translator app powered by vue.js

---
+++ src/components/TranslateOutput.vue
@@ -0,0 +1,15 @@
+<template>
+<h1 class="text-info">{{translatedText}}</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'TranslateOutput',
+  props: ['translatedText']
+}
+</script>
+
+<style>
+
+</style>
